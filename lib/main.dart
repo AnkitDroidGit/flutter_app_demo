3341af54b1785553554ec99a31e96a35dd0b4805
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-08-11 17:54:56 +0800

    Added icons to the list

--- lib/main.dart
+++ lib/main.dart
@@ -19,18 +19,21 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];            // NEW
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+  final _suggestions = <WordPair>[]; // NEW
+  final _biggerFont = const TextStyle(fontSize: 18);
+  final _saved = Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold (                     // Add from here...
+    return Scaffold(
+      // Add from here...
       appBar: AppBar(
         title: Text('Startup Name Generator'),
       ),
       body: _buildSuggestions(),
     );
   }
+
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
@@ -61,14 +64,19 @@ class _RandomWordsState extends State<RandomWords> {
             _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
-        }
-    );
+        });
   }
+
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(   // NEW from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
